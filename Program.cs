7cfc79b969e4329e184515deffbfab2a0cf618cc
CODE WITH JOSE DAVID <48775594+JoseCode-CO@users.noreply.github.com>
2024-02-19 21:54:52 -0500

    Casting

--- Program.cs
+++ Program.cs
@@ -15,12 +15,50 @@ class HelloWord{
         char myLetter = 'D';         // Character
         bool myBool = true;          // Boolean
         string myText = "Hello";     // String
-        
+
         Console.WriteLine(myLetter);
 
         //Constantes, esta va a permitir que cuando se defina una variable no se le pueda reasignar su valors
         const int ages = 15;
         //myNum = 20;
+
+
+        //En C#, existen dos tipos de conversión:
+
+        /*Conversión implícita (automáticamente): conversión de un tipo más pequeño a un tamaño de tipo más grande
+        char-> int-> long-> float->double
+
+        Conversión explícita (manualmente): convertir un tipo más grande en un tipo de tamaño más pequeño
+        double-> float-> long-> int->char*/
+
+        //Implicita
+
+        int myInt = 92;
+        double myDouble = myInt;      
+
+        Console.WriteLine(myInt);      
+        Console.WriteLine(myDouble);   
+
+        //Explicita
+        double myDouble2 = 9.78;
+        int myInt2 = (int) myDouble2;    
+
+        Console.WriteLine(myDouble2);   
+        Console.WriteLine(myInt2);    
+
+
+        int myInt3 = 10;
+        double myDouble3 = 5.25;
+        bool myBool3 = true;
+
+        Console.WriteLine(Convert.ToString(myInt3));    // convert int to string
+        Console.WriteLine(Convert.ToDouble(myInt3));    // convert int to double
+        Console.WriteLine(Convert.ToInt32(myDouble3));  // convert double to int
+        Console.WriteLine(Convert.ToString(myBool3));   // convert bool to string  
+
+        string myString = "Hello";
+        Console.WriteLine(myString[0]);  // Outputs "H"
+
     }
   
 }
--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d585ccec54ccf4524f15f69a65497a2cd216c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c1308a1125007eed5db8e117326bbbb0636d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
